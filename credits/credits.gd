06c229f1e8b6865f9aab5e8ce63b22c22c54b4e2
Francisco Falcao <fcodavibandeira@edu.unifor.br>
2024-03-04 11:11:47 -0300

    adding credits script (to not forget)

---
+++ credits/credits.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name Credits
+
+var credits_data: Dictionary = {
+	"Pixelation Shader": {
+		"User": "Seraphon",
+		"Link": "https://godotshaders.com/shader/3d-pixelation-filter/"
+	},
+	
+	"Based Outline Shader": {
+		"User": "OlisUnfinishedProjects",
+		"Link": "https://godotshaders.com/shader/thick-3d-screen-space-depth-normal-based-outline-shader/"
+	},
+	
+	"3D Models": {
+		"User": "Kay Lousberg",
+		"Link": "https://kaylousberg.itch.io/"
+	}
+}
